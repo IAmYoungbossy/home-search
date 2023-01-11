9bd75148692260881baff3f1b616578d95fb710c
Letam Bossman Barinua <letambarinua@gmail.com>
2023-01-11 19:52:21 +0100

    Add NavLinks component to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,21 +4,72 @@ import styled from "styled-components";
 export default function Header() {
   return (
     <header>
-      <HeaderLogo />
+      <HeaderLogoAndNav />
     </header>
   );
 }
 
+// Header logo and Nav links container
+const StyledHeaderLogoAndNav = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > div:first-of-type {
+    margin-right: 30px;
+  }
+`;
+function HeaderLogoAndNav() {
+  return (
+    <StyledHeaderLogoAndNav>
+      <HeaderLogo />
+      <NavLinks />
+    </StyledHeaderLogoAndNav>
+  );
+}
+
+// Header Logo components
 const StyledHeaderLogo = styled.div`
-  img {
+  & > img {
     width: 150px;
   }
 `;
-
 function HeaderLogo() {
   return (
     <StyledHeaderLogo>
       <img src={Logo} alt="Logo" />
     </StyledHeaderLogo>
+  );
+}
+
+// Nav links components
+const navLinks = [
+  "Buy",
+  "Sell",
+  "Rent",
+  "Mortgage",
+  "Find Realtors",
+  "My Home",
+  "News & Insights",
+];
+const StyledNavLinks = styled.nav`
+  & > ul {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > li {
+      list-style: none;
+    }
+  }
+`;
+function NavLinks() {
+  return (
+    <StyledNavLinks>
+      <ul>
+        {navLinks.map((link) => (
+          <li>{link}</li>
+        ))}
+      </ul>
+    </StyledNavLinks>
   );
 }
