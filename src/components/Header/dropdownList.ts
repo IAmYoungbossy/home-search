b55fcfd5282e8a8b56400eb503357c10f069e7a2
Letam Bossman Barinua <letambarinua@gmail.com>
2023-01-12 14:55:53 +0100

    Add dropdownList module containing item list

---
+++ src/components/Header/dropdownList.ts
@@ -0,0 +1,246 @@
+export const dropdownList = [
+  {
+    LinkName: "Buy",
+    Lists: [
+      [
+        {
+          Section: "Homes for Sale",
+          lists: [
+            "Surprise Homes For Sale",
+            "Surprise Foreclosures",
+            "Surprise Open Houses",
+            "New Construction For Sale",
+            "hr",
+            "All Surprise New Construction",
+            "Surprise New Home Communities",
+            "Surprise New Construction and Plans",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Homes Values",
+          lists: [
+            "Surprise Housing Market",
+            "Surprise Recently Sold",
+            "Surprise Property Records",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Home Buying Tips",
+          lists: [
+            "Home Buying Checklist",
+            "Home Buyers Reveal: 'What I Wish I Had Known Before Buying My First Home",
+            "First-Time Home Buyer Resource Center",
+            "More Home Buying Insights",
+            "Success Stories",
+          ],
+        },
+      ],
+    ],
+  },
+  {
+    LinkName: "Sell",
+    Lists: [
+      [
+        {
+          Section: "Home selling tools",
+          lists: [
+            "Find the right selling option for you",
+            "Compare top agents with UpNest",
+            "See What Your Home Is Worth",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Home selling advice",
+          lists: [
+            "Complete Guide To Selling Your Home",
+            "hr",
+            "Should I Sell My Home Now?",
+            "How Much Is My Home Worth?",
+            "How Should I Sell My Home?",
+            "How To Select An Agent?",
+            "How To Prepare Your Home for Sale?",
+            "hr",
+            "See more home selling insights",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Recently sold",
+          lists: ["Recently sold homes in Surprise"],
+        },
+      ],
+    ],
+  },
+  {
+    LinkName: "Rent",
+    Lists: [
+      [
+        {
+          Section: "Apartments For Rent",
+          lists: [
+            "Apartments for Rent in Surprise",
+            "Houses for Rent in Surprise",
+            "All Surprise Rentals",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Landlord Tools",
+          lists: [
+            "Manage Rentals",
+            "List Your Rentals",
+            "Screen Tenants",
+            "Create A Lease",
+            "Collect Rent Online",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Home Renting Tips",
+          lists: [
+            "Renter's Resource Center",
+            "Should I Rent Or Buy?",
+            "Debunked! 8 Myths About Renting You Should Stop Believing Immediately",
+            "hr",
+            "Rental Report",
+            "More Renting Insights",
+          ],
+        },
+      ],
+    ],
+  },
+  {
+    LinkName: "Mortgage",
+    Lists: [
+      [
+        {
+          Section: "Mortgage Rates",
+          lists: [
+            "Get Pre-Approved",
+            "Surprise Mortgage Rates",
+            "Home Equity Financing Rates",
+            "Refinance Rates",
+            "Finance Advice",
+            "For Veterans",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Calculators",
+          lists: [
+            "Mortgage Calculator",
+            "Refinance Calculator",
+            "How Much House Can I Afford",
+            "Rent vs. Buy",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Financial Advice",
+          lists: [
+            "6 Ways Home Buyers Mess Up Getting A Mortgage",
+            "Mortgage Guide",
+            "Learn About Home Insurance",
+            "More Finance Insights",
+          ],
+        },
+      ],
+    ],
+  },
+  {
+    LinkName: "Find Realtors",
+    Lists: [
+      [
+        {
+          Section: "Find REALTORS",
+          lists: [
+            "Top Rated Surprise REALTORS",
+            "Search For Real Estate Agent",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Why Use A REALTOR",
+          lists: [
+            "6 Reasons You Should Never Buy Or Sell A Home Without An Agent",
+            "Difference Between Agent, Broker & Realtor",
+            "Listing vs. Buyer Agent",
+            "How To Find A REALTOR®",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "News Around REALTORS",
+          lists: [
+            "Real Estate Agents Reveal the Toughest Home Buyers They've Ever Met",
+            "More News Around REALTORS®",
+          ],
+        },
+      ],
+    ],
+  },
+  {
+    LinkName: "News & Insights",
+    Lists: [
+      [
+        {
+          Section: "News",
+          lists: [
+            "The Latest News",
+            "Housing Trends",
+            "Real Estate News",
+            "Celebrity Real Estate",
+            "Unique Homes",
+            "For Pros",
+            "Corporate Blog",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Insights",
+          lists: [
+            "Buying",
+            "Selling",
+            "Renting",
+            "Financing",
+            "Moving",
+            "Living",
+            "Home Improvement",
+            "Research",
+            "2023 Housing Market Predictions",
+            "2022 Hottest Zip Codes",
+          ],
+        },
+      ],
+      [
+        {
+          Section: "Guides & More",
+          lists: [
+            "All Guides",
+            "Complete Guide on How to Sell your Home",
+            "First-Time Home Buyer Resource Center",
+            "Mortgage Guide",
+            "Veterans Home Buyer Guide",
+            "Home Insurance Guide",
+            "Real Estate Videos",
+            "Housing Resources",
+          ],
+        },
+      ],
+    ],
+  },
+];
