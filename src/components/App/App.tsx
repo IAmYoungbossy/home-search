06ba2cc8a1c6b1ccd5197663ec00e97e6c6d6859
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-16 22:22:44 +0100

    Remove Realtors clone page to make it a seperate project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,27 +11,19 @@ import Comment, { commentLoader } from "../Comment/Comment";
 import { contextProps } from "../../utilities/typesAndInitialStateObj";
 import SocialPageLayout, { SocialPage } from "../SocialPage/SocialPage";
 import CreatePostPage, { editPostLoader } from "../CreatePost/CreatePost";
-import LandingPage, { LandingPageLayout } from "../LandingPage/LandingPage";
 import { onClickToggleButtonTags } from "../../utilities/createPostHelperFn";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<LandingPageLayout />}>
-      <Route index element={<LandingPage />} />
-      <Route path="social" element={<SocialPageLayout />}>
-        <Route index element={<SocialPage />} loader={postLoader} />
-        <Route
-          path="edit/:id"
-          element={<CreatePostPage />}
-          loader={editPostLoader}
-        />
-        <Route path="post" element={<CreatePostPage />} />
-        <Route
-          path="comment/:id"
-          element={<Comment />}
-          loader={commentLoader}
-        />
-      </Route>
+    <Route path="/" element={<SocialPageLayout />}>
+      <Route index element={<SocialPage />} loader={postLoader} />
+      <Route
+        path="edit/:id"
+        element={<CreatePostPage />}
+        loader={editPostLoader}
+      />
+      <Route path="post" element={<CreatePostPage />} />
+      <Route path="comment/:id" element={<Comment />} loader={commentLoader} />
     </Route>
   )
 );
