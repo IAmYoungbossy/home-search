bad93a623fa96dffcf89efb5d4ea14e473dc2cd0
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-03 19:59:39 +0100

    Add logic to logout or login/sign-up depending on if user is signed in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,6 +16,7 @@ import { AppContext } from "../../context/AppContext";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { buttonContent } from "../../constant/textConstant";
 import { LOG_IN_PANEL } from "../../constant/objectConstant";
+import { logout } from "../../firebaseAuth";
 
 export function Header() {
   return (
@@ -79,15 +80,6 @@ export function CTAButtons() {
 }
 
 export function UserIcon() {
-  const { dispatch, state } = useContext(
-    AppContext
-  ) as contextProps;
-
-  const { signInToggle } = signInObj(state);
-
-  const handleSignInPageToggle = () =>
-    dispatch(signInToggle);
-
   return (
     <SC.StyledUserIcon>
       <HeartSVG />
@@ -132,14 +124,43 @@ function UserButton() {
 }
 
 function LoginPanel() {
+  const { dispatch, state } = useContext(
+    AppContext
+  ) as contextProps;
+
+  const { signInToggle } = signInObj(state);
+
+  const handleSignInPageToggle = () =>
+    dispatch(signInToggle);
+
+  const logoutOrLogin = () => {
+    if (state.user) logout();
+    else handleSignInPageToggle();
+  };
+
   return (
-    <SC.StyledLoginPanel>
+    <SC.StyledLoginPanel
+      onClick={(e) => e.stopPropagation()}
+    >
       {LOG_IN_PANEL.map((item) => {
         if (item.arrow) {
           return (
             <button key={item.name}>
               {item.icon} <span>{item.name}</span>{" "}
               {item.arrow}
+            </button>
+          );
+        }
+        if (item.name === "Log In / Sign Up") {
+          return (
+            <button
+              key={item.name}
+              onClick={logoutOrLogin}
+            >
+              {item.icon}{" "}
+              <span>
+                {state.user ? "Logout" : `${item.name}`}
+              </span>
             </button>
           );
         }
