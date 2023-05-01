0601807c9a358b3e681ef86d5b191cd76db02874
Letam Bossman Barinua <letambarinua@gmail.com>
2023-05-01 08:38:22 +0100

    Create a seperate folder and module for routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,51 +1,9 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import { useContext } from "react";
-
-import CreateOrEditPost, {
-  editPostLoader,
-} from "../../pages/CreateOrEditPost";
-import { postLoader } from "../Cards/PostCards";
-import { HomeSearch } from "../../pages/HomeSearch";
+import router from "../../routes/routes";
+import { RouterProvider } from "react-router-dom";
 import { contextProps } from "../../utilities/types";
 import { AppContext } from "../../context/AppContext";
-import Comment, { commentLoader } from "../../pages/Comment";
-import HomeSearchLayout from "../../Layouts/HomeSearchLayout";
 import { onClickToggleButtonTags } from "../../utilities/helper";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      path="/"
-      element={<HomeSearchLayout />}
-    >
-      <Route
-        index
-        element={<HomeSearch />}
-        loader={postLoader}
-      />
-      <Route
-        path="edit-post/:id"
-        element={<CreateOrEditPost />}
-        loader={editPostLoader}
-      />
-      <Route
-        path="create-post"
-        element={<CreateOrEditPost />}
-      />
-      <Route
-        path="comment/:id"
-        element={<Comment />}
-        loader={commentLoader}
-      />
-    </Route>
-  ),
-  { basename: "/" }
-);
 
 function App() {
   const { dispatch } = useContext(AppContext) as contextProps;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,44 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import CreateOrEditPost, {
+  editPostLoader,
+} from "../pages/CreateOrEditPost";
+import { HomeSearch } from "../pages/HomeSearch";
+import Comment, { commentLoader } from "../pages/Comment";
+import HomeSearchLayout from "../Layouts/HomeSearchLayout";
+import { postLoader } from "../components/Cards/PostCards";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<HomeSearchLayout />}
+    >
+      <Route
+        index
+        element={<HomeSearch />}
+        loader={postLoader}
+      />
+      <Route
+        path="edit-post/:id"
+        element={<CreateOrEditPost />}
+        loader={editPostLoader}
+      />
+      <Route
+        path="create-post"
+        element={<CreateOrEditPost />}
+      />
+      <Route
+        path="comment/:id"
+        element={<Comment />}
+        loader={commentLoader}
+      />
+    </Route>
+  ),
+  { basename: "/" }
+);
+
+export default router;
