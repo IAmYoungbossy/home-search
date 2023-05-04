be4897e2de6d5e7b38d2301b95178907aa5d1893
Letam Bossman Barinua <letambarinua@gmail.com>
2023-05-04 17:46:26 +0100

    Create spinner when posting or editing

--- src/components/CreatePost/CreateOrEditPostButton.tsx
+++ src/components/CreatePost/CreateOrEditPostButton.tsx
@@ -1,20 +1,34 @@
-import Tags from "./Tags";
 import {
   editPost,
   preventEmptyFieldSubmition,
 } from "../../utilities/helper";
-import { useContext, useEffect } from "react";
+
 import {
   contextProps,
   APP_ACTION_TYPES,
 } from "../../utilities/types";
+
+import Tags from "./Tags";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context/AppContext";
+import styled, { keyframes } from "styled-components";
 import { StyleActionButtons } from "./StyledCreatePost";
 import { useNavigate, useParams } from "react-router-dom";
 import { findUser } from "../../firebase/firebaseCRUD/checkIfOldUser";
 import { AddPostToDB } from "../../firebase/firebaseCRUD/addPostToFirestore";
 
+const Spinner = () => (
+  <StyledSpinner>
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </StyledSpinner>
+);
+
 export default function CreateOrEditPostButton() {
+  const [loadSpinner, setLoadSpinner] = useState(false);
   const editId = useParams().id;
   const navigate = useNavigate();
   const { state, dispatch } = useContext(
@@ -50,6 +64,7 @@ export default function CreateOrEditPostButton() {
       }, 5000);
       return;
     }
+    setLoadSpinner(true);
     AddPostToDB(state, editId, dispatch, navigate);
   };
 
@@ -58,15 +73,75 @@ export default function CreateOrEditPostButton() {
   return (
     <StyleActionButtons bg={allFieldsFilled}>
       <Tags />
-      <div>
+      <ButtonWrapper>
         {state.postType.trim() !== "" && (
           <small>{state.postType}</small>
         )}
         <button>Save Draft</button>
-        <button onClick={handleCreateOrEditPost}>
-          {!editId ? "Create Post" : "Save Edit"}
-        </button>
-      </div>
+        <Button
+          onClick={handleCreateOrEditPost}
+          disabled={loadSpinner}
+        >
+          {loadSpinner ? (
+            <Spinner />
+          ) : !editId ? (
+            "Create Post"
+          ) : (
+            "Save Edit"
+          )}
+        </Button>
+      </ButtonWrapper>
     </StyleActionButtons>
   );
 }
+
+const Button = styled.button`
+  width: 95px;
+  height: 25px;
+  position: relative;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+`;
+
+const keyFrame = keyframes`
+0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }`;
+
+const StyledSpinner = styled.div`
+  top: 2px;
+  width: 20px;
+  right: 36px;
+  height: 20px;
+  position: absolute;
+  display: inline-block;
+
+  & div {
+    width: 19px;
+    height: 19px;
+    display: block;
+    position: absolute;
+    border-radius: 50%;
+    box-sizing: border-box;
+    border: 1px solid #fff;
+    border-color: #fff transparent transparent transparent;
+    animation: ${keyFrame} 1.2s cubic-bezier(0.5, 0, 0.5, 1)
+      infinite;
+  }
+  & div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  & div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  & div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`;
